adfa1f5562f672e6e805c3e11c3b4bd4391d4b4e
Juan <Erikjuan32@gmail.com>
2025-08-20 19:44:47 -0300

    update responsitividade

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function AbrirMenu() {
-    const M = document.getElementById("menu");
+    const logo = document.getElementById("logoclt");
     const Nav = document.getElementById("MenuNav");
 
     if(Nav.style.display == "flex"){
         Nav.style.display = "none";
+        logo.style.display = "block";
     } else {
         Nav.style.display = "flex";
         Nav.style.justifyitems = "center";
+        logo.style.display = "none";
     }
 }
